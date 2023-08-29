617d08620a6b7ee9129cf10e3fb1674e8cfc21ae
leonardo <correabrasilleonardo@gmail.com>
2023-08-28 21:35:07 -0300

    atualizando

--- dashboard.php
+++ dashboard.php
@@ -3,7 +3,58 @@
 ?>
 
 
-    <h1>Dashboard</h1>
+     <div id="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Gerenciar pedidos</h2>
+                </div>
+                <div class="col-md-12 table-container">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col"> <span>Pedido</span> #</th>
+                                <th scope="col">Borda</th>
+                                <th scope="col">Massa</th>
+                                <th scope="col">Sabores</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Açoes</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>#1</td>
+                                <td>Chedar</td>
+                                <td>Catupery</td>
+                                <td>4 Queijos</td>
+                                <td>
+                                    <form action="process/orders.php" method=" POST" class="form-group update-form">
+                                        <input type="hidden" name="type" value="update">
+                                        <input type="hidden" name="id" value="1">
+                                        <select name="status" class="form-control status-input">
+                                            <option value="">Entrega</option>
+                                        </select>   
+                                        <button type="submit" class="update-btn">
+                                            <i class="fas fa-sync-alt"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                                <td>
+                                    <form action="process/orders.php" method="POST">
+                                        <input type="hidden" name="type" value="delete">
+                                        <input type="hidden" name="id" value="1">
+                                        <button type="submit" class="delete-btn">
+                                            <i class="fas fa-times></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+     </div>
     
 
 <?php
--- process/conn.php
+++ process/conn.php
@@ -2,12 +2,17 @@
 
 session_start();
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 $user = "root";
 $pass = "leonardo";
 $db = 'pizzaria';
 $host = 'localhost';
 
 try {
+
+   
 
     $conn = new PDO("mysql:host={$host};dbname={$db}",$user,$pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
